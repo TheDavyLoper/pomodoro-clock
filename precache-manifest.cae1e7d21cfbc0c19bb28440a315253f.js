34b6076d30429643fad21b5ff58aef72ae164177
David Akpan <daviduwana@gmail.com>
2020-05-27 01:30:50 +0100

    Updates

---
+++ precache-manifest.cae1e7d21cfbc0c19bb28440a315253f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c040e7e8a719fd92ac65",
+    "url": "/pomodoro-clock/static/css/main.1a0ab86d.chunk.css"
+  },
+  {
+    "revision": "c040e7e8a719fd92ac65",
+    "url": "/pomodoro-clock/static/js/main.c040e7e8.chunk.js"
+  },
+  {
+    "revision": "d439e8744bee3815eb5d",
+    "url": "/pomodoro-clock/static/js/1.d439e874.chunk.js"
+  },
+  {
+    "revision": "f41d346ce9ebc121a229",
+    "url": "/pomodoro-clock/static/js/runtime~main.f41d346c.js"
+  },
+  {
+    "revision": "7e36bbad8b145db2c65c41404f369b41",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
